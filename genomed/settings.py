48bc07f78157a2e58927ea7815d7e81dddf33f0f
reydon <dhruvarora@delhinerds.com>
2017-11-28 11:43:20 +0530

    deplouing

--- genomed/settings.py
+++ genomed/settings.py
@@ -128,3 +128,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-files')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media-files')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
